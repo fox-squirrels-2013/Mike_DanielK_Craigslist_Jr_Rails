6e7abdcc5cb64ee99809f2b7761a2cac16cced98
theinternauts <cricketw@wearetheinternauts.com>
2013-11-18 12:33:26 -0800

    Started project with models and migrations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClJr::Application.config.secret_token = '640d9bba7a40a5f1820aeef1b269affc8a38d09aca9b7db7c7488455ed987b82a9e5a59ea6c78163a0e5526a8568805fc6d0857920a6d5b9955d489d018bd503'
